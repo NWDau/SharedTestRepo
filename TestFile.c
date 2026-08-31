57d84831bb6a27d321845ca80163b3e73265c735
BartoutAdam <adamjr11@NW59815.nwmissouri.edu>
2026-08-31 12:29:42 -0500

    wehhd

---
+++ TestFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() // or int main(int argc, char* argv[])
+{
+    int x = 10;
+    int y; // declaring the variable y
+    y = 15; // initialization, but for functions is: definition
+    puts("We are..");
+    puts("Hello World!");
+    printf("x is %d\n", x);
+    return 0;
+}
